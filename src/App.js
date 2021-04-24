360ce8a95538ecf8ac40c3415c8b009544d25f2a
vaibhavmahindru <500067728@stu.upes.ac.in>
2021-04-25 04:04:09 +0530

    firebase added and basic setup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionsData} from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAlZdrzLWlEQKOjq5trMfqan_7niTWwk10",
+  authDomain: "chat-app-8c5d5.firebaseapp.com",
+  projectId: "chat-app-8c5d5",
+  storageBucket: "chat-app-8c5d5.appspot.com",
+  messagingSenderId: "113050167222",
+  appId: "1:113050167222:web:d1811b87584f93d1f4c317",
+  measurementId: "G-1KF8FG72FM"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+const analytics = firebase.analytics();
 function App() {
+  
+  const[userLoggedIn] = useAuthState(auth);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <section>
+        {userLoggedIn ? <ChatRoom/> : <SignIn/>}
+        </section>
     </div>
   );
 }
